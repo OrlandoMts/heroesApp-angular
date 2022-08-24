3fccdb8ceee9ac4a354b1f849e526fccb1ab6f3f
Orlando Montes <omontes@indelpro.com>
2022-08-24 16:21:12 -0500

    implemente lazyload y cree las rutas hijas para el modulo heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
@@ -12,7 +14,9 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // Solo importar el router principal, los deas seran cargados mediante lazyLoad
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
